9c0ce20fe15ae5747a4f29b144e6188f086205f1
Srikant Sarwa <ssarwa@microsoft.com>
2019-01-23 16:03:09 -0800

    corrected server variable

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var http = require("http");
-http.createServer(function (request, response) {
+var server = http.createServer(function (request, response) {
     // Send the HTTP header 
     // HTTP Status: 200 : OK
     // Content Type: text/plain
